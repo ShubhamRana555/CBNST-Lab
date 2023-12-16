135c688602b6e538a05d607a7a99899b52cd1f14
Shubham <shubhamrana0108@gmail.com>
2023-12-16 09:53:10 +0530

    added regression line program

--- 18_linearCurveFitting.c
+++ 18_linearCurveFitting.c
@@ -1,58 +1,55 @@
-//C Program to Fit a straight line on given points
+// Write a program in "C" Language for Linear Curve Fitting
 
 #include<stdio.h>
+
+void gaussjordon(float arr[10][10],int n)
+{
+    float ratio,x[10];
+    for(int i=1;i<=n;i++)
+    {
+        if(arr[i][i]==0.0)
+           {
+            printf("Math Error");
+            break;
+           }
+        for(int j=1;j<=n;j++){
+            if(i!=j){
+                ratio =arr[j][i]/arr[i][i];
+                for(int k=1;k<=n+1;k++)
+                   arr[j][k]=arr[j][k]-ratio*arr[i][k];
+            }
+        }
+    }
+    for(int i=1;i<=n;i++)
+        x[i]=arr[i][n+1]/arr[i][i];
+    
+    float a=x[1],b=x[2];
+    printf("Equation: y= %.2f + %.2fx\n", a, b);
+
+}
 int main()
 {
     int n;
-    float sum1=0,sum2=0,sum3=0,sum4=0,a,b;
-    //.............Input...................
-    printf("Enter no. of observations\n");
+    float sumx=0,sumy=0,sumx2=0,sumxy=0;
+    printf("Enter no of points:");
     scanf("%d",&n);
-    float x[n],y[n],augmented_matrix[2][3];
-    printf("Enter values of x\n");
+    float x[n],y[n],table[10][10];
+    printf("Enter points:\n");
     for(int i=0;i<n;i++)
-      scanf("%f",&x[i]);
-    printf("Enter values of y\n");
-    for(int i=0;i<n;i++)
-      scanf("%f",&y[i]); 
-
-    //....................Computations ............
+      {
+         scanf("%f%f",&x[i],&y[i]);
+      }
     for(int i=0;i<n;i++)
     {
-        sum1=sum1+x[i];
-        sum2=sum2+y[i];
-        sum3=sum3+x[i]*y[i];
-        sum4=sum4+x[i]*x[i];
+        sumx+=x[i];
+        sumy+=y[i];
+        sumx2+=x[i]*x[i];
+        sumxy+=x[i]*y[i];
     }
 
-    //finding slope and intercept
-    augmented_matrix[0][0]=n;
-    augmented_matrix[0][1]=sum1;
-    augmented_matrix[0][2]=sum2;
-    augmented_matrix[1][0]=sum1;
-    augmented_matrix[1][1]=sum4;
-    augmented_matrix[1][2]=sum3;
+    table[1][1]=n,table[1][2]=sumx,table[1][3]=sumy;
+    table[2][1]=sumx,table[2][2]=sumx2,table[2][3]=sumxy;
 
-    //solving it
-    int ratio=augmented_matrix[1][0]/augmented_matrix[0][0];
-    for(int i=0;i<3;i++)
-        augmented_matrix[1][i]=augmented_matrix[1][i]-ratio*augmented_matrix[0][i];
-    
-    //printing Upper Triangular matrix
-    printf("\nThe Upper Triangular Matrix:\n");
-    for(int i=0;i<2;i++)
-    {
-        for(int j=0;j<3;j++)
-          printf("%.2f ",augmented_matrix[i][j]);
-        printf("\n");  
-    }
-
-    //finding a and b by back substitution (a = intercept , b = slope)
-    b=augmented_matrix[1][2]/augmented_matrix[1][1];
-    a=(augmented_matrix[0][2]-augmented_matrix[0][1]*b)/augmented_matrix[0][0];
-    printf("\nIntercept = %.2f and Slope = %.2f\n\n",a,b); 
-    //..........................Output...................
-    printf("Equation of The line: y= %.2f + %.2fx",a,b);
-
-
+    gaussjordon(table,2);
+    return 0;
 }
